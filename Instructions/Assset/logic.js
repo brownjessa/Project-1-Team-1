bdffa974907216927922211c883d76eb16ea0490
Osyrus85 <osyrus85@gmail.com>
2020-06-26 14:17:50 -0500

    base js

--- Instructions/Assset/logic.js
+++ Instructions/Assset/logic.js
@@ -1,37 +1,34 @@
 // article key70d33a5bb880c7ea27d6d510a14fac55
 $(document).ready(function () {
-    
-    
-    
-    
-    $("button").on("click", function() {
 
-    
-    
-    
-    
-    
-    
-    
-    
-        function displayArticleInfo() {
 
-        var article = $(this).attr("display-article");
-        var queryURL ="https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=KA8lDAriISCJyVV0ouUHqjPUROdmn2nc"; 
 
-        
-        
-        
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response) {
-          $("#display-article").text
-        console.log(response);
-        });
 
-      } 
+    $("button").on("click", function () {
 
 
 
     });
+        function displayArticleInfo() {
+
+            var article = $(this).attr("display-article");
+            var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=KA8lDAriISCJyVV0ouUHqjPUROdmn2nc";
+
+
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+
+                console.log(response);
+                
+            });
+
+        }
+    });
+
+
+
+
+
+
